b122f8ea5d4336b29d9734cff5f5fa2320670e62
David Guan <DavidGuanDev@gmail.com>
2016-04-23 18:25:45 +0800

    Implement drag behavior.

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,17 @@ import './home.scss'
 const containerDom = document.querySelector('#app')
 const colors = d3.scale.category10()
 
+const drag = d3.behavior.drag()
+    .origin(d => d)
+    .on("drag", dragmove);
+
+function dragmove() {
+  const x = d3.event.x
+  console.log(x)
+  d3.select(this)
+    .style('transform', `translate(${d3.event.x}px, ${d3.event.y}px)`)
+}
+
 const data = {
   todo: [
     'datavis',
@@ -18,9 +29,11 @@ const data = {
     'QA #111',
   ]
 }
+//That is the reason why we need put x, y here
+//https://stackoverflow.com/questions/13657687/drag-behavior-returning-nan-in-d3
 const mockData =[
-  [1, 2],
-  [3, 4],
+  [{ text: 1, x: 0, y: 0 }, { text: 2, x: 0, y: 50 }],
+  [{ text: 3, x: 0, y: 0 }, { text: 4, x: 0, y: 50 }],
 ]
 class Board extends Component {
   componentDidMount() {
@@ -37,18 +50,20 @@ class Board extends Component {
         .data(mockData[i])
         .enter()
         .append('g')
+        .attr("x", function(d,i){console.log(d)})
+        .style('transform', d => `translate(0, ${d.y}px)`)
+        .call(drag)
 
       card.append('rect').style({
         width: '50px',
         height: '30px',
         fill: colors(i),
-        transform: (_, ind) => `translate(0, ${ 50 * ind }px)`,
       })
 
       card.append('text').style(
-          'transform', (_, ind) => `translate(10px, ${ 50 * ind + 20}px)`,
+          'transform', (_, ind) => `translate(10px, 20px)`,
         )
-        .text(d => d)
+        .text(d => d.text)
     })
   }
   render() {
